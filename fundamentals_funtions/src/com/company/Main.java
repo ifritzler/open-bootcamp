8ca5e79d1e8805b1c8e38fded45026dcc36aeec9
Ilan Emanuel Fritzler <contacto.fritzlerilan@gmail.com>
2022-03-28 00:20:23 -0300

    First exercise - Function called from main

--- fundamentals_funtions/src/com/company/Main.java
+++ fundamentals_funtions/src/com/company/Main.java
@@ -3,7 +3,10 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-
+        /* First Exercise*/
+        int resultado;
+        resultado = sumatoriaTriple(4,5,6);
+        System.out.println(resultado);
     }
 
     public static int sumatoriaTriple(int a, int b, int c){
