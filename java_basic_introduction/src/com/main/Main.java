f3f854724effb074a64e459de9feed63018aac64
Ilan Emanuel Fritzler <contacto.fritzlerilan@gmail.com>
2022-04-03 10:38:39 -0300

    Introduccion a Java - Ejercicio 1

---
+++ java_basic_introduction/src/com/main/Main.java
@@ -0,0 +1,32 @@
+package com.main;
+
+public class Main {
+    /**
+     * Metodo principal de inicio del programa
+     * @param args Argumentos de entrada.
+     */
+    public static void main(String[] args) {
+        byte _byte = 1;
+        short _short = 400;
+        int _int = 1000;
+        long _long = 100000;
+
+        System.out.println("Byte: " + _byte + "; Short " + _short + "; Int: " + _int + "; Long: " + _long);
+
+        float _float = 1.9f;
+        double _double = 2.342313D;
+
+        System.out.println("Float: " + _float + "; Double: " + _double);
+
+        boolean _false = false;
+        boolean _true = true;
+
+        System.out.println("Boolean False: " + _false + "; Boolean true: " + _true);
+
+        char _char = 'c';
+        String string = "hello world";
+
+        System.out.println("Char: " + _char + "; String: " + string);
+
+    }
+}
